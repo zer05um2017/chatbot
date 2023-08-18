da3bb1d0f53ad3b49f7fca806d5661357efcbd14
zer05um2017 <perry9122@gmail.com>
2023-08-18 16:54:56 +0900

    bug fixed

--- backend/core.py
+++ backend/core.py
@@ -1,9 +1,10 @@
 import os
 # import sys
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import pinecone
 from typing import Any, List, Tuple
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain import PromptTemplate
 from langchain.vectorstores import Pinecone
@@ -12,14 +13,17 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.schema import HumanMessage, SystemMessage
 from consts import INDEX_NAME
 
-template_message = [
-    SystemMessage(
-        content="You are a health care consultant and your role is to answer customer inquiries."
-    ),
-    HumanMessage(
-        content="I can't digest these days. What kind of test should I take?"
-    ),
-]
+load_dotenv()
+
+# template_message = [
+#     SystemMessage(
+#         content="You are a health care consultant and your role is to answer customer inquiries."
+#     )
+# ]
+
+system_message_prompt = PromptTemplate.from_template(
+    template="""You are a health care consultant and your role is to answer customer inquiries in Korean language."""  
+)
 
 pinecone.init(
     api_key=os.environ.get("PINECONE_API_KEY"),
@@ -34,9 +38,11 @@ def run_llm(query: str, chat_history: List[dict[str, Any]] = []) -> Any:
     )
     chat = ChatOpenAI(verbose=True, temperature=0, model="gpt-3.5-turbo")
 
-    _template = """Given the following conversation and a follow up question, 
+    _template = """
+    You are a health care consultant and your role is to answer customer inquiries in Korean language.
+    Given the following conversation and a follow up question, 
     rephrase the follow up question to be a standalone question. 
-    At the end of standalone question add this 'Answer the question in Korean language.' If you do not know the answer reply with 'I am sorry' in Korean.
+    At the end of standalone question add this 'Answer the question' If you do not know the answer reply with 'I am sorry' in Korean.
     Chat History:
     {chat_history}
     Follow Up Input: {question}
@@ -45,7 +51,7 @@ def run_llm(query: str, chat_history: List[dict[str, Any]] = []) -> Any:
 
     qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
-        template_message=template_message,
+        # system_prompt=system_prompt,
         retriever=docsearch.as_retriever(),
         return_source_documents=True,
         condense_question_prompt=CONDENSE_QUESTION_PROMPT,
